047ea435494fffbe1c38a110cef2c850c62f4604
heonn <najh0094@gmail.com>
2023-02-11 14:58:54 +0900

    init

---
+++ src/main/java/jpaporj/entity/Main.java
@@ -0,0 +1,14 @@
+package jpaporj.entity;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
+		
+	}
+
+}
---
+++ src/main/java/jpaporj/entity/OrderStatus.java
@@ -0,0 +1,6 @@
+package jpaporj.entity;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+
+}
