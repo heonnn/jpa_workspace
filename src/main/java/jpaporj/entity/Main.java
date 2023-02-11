7de5df97e67ea8d7b84c5544efff5d2bb9c999bd
heonn <najh0094@gmail.com>
2023-02-11 15:10:33 +0900

    feat: 2023.02.11 db연결

--- src/main/java/jpaporj/entity/Main.java
+++ src/main/java/jpaporj/entity/Main.java
@@ -2,6 +2,7 @@ package jpaporj.entity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
@@ -10,6 +11,22 @@ public class Main {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
 		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		
+		try {
+			tx.begin();
+
+			Member member = new Member();
+			member.setName("test");
+			
+			em.persist(member);
+			tx.commit();
+		} catch(Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
 	}
 
 }
