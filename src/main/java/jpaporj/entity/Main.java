77d546abfbac1dfbca06633f31259aac1eca7c2e
heonn <najh0094@gmail.com>
2023-02-11 15:09:35 +0900

    feat: 2023.02.11 db연결

--- src/main/java/jpaporj/entity/Main.java
+++ src/main/java/jpaporj/entity/Main.java
@@ -1,5 +1,6 @@
 package jpaporj.entity;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
-		
+		EntityManager em = emf.createEntityManager();
 	}
 
 }
