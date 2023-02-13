78606814a73c038116208a7d6785399c94438412
jhkim5981 <jhkim5981@raoncorp.com>
2023-02-13 18:26:57 +0900

    feat: 2023.02.13 hibernate, jdk 버전 변경. db연동 완료

--- src/main/java/jpaporj/Main.java
+++ src/main/java/jpaporj/Main.java
@@ -1,9 +1,11 @@
-package jpaporj.entity;
+package jpaporj;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import jpaporj.entity.Member;
 
 public class Main {
 
